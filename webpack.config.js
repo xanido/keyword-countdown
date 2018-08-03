7a84b2bd96ffb644f935fec770b2f8e51c0dfe6a
Tim Anido <tim@pictura.com.au>
2018-08-03 11:26:42 +1000

    Add more babel transforms, css & env plugins for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -18,9 +19,32 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader", // compiles Sass to CSS, using Node Sass by default
+            {
+                loader: 'sass-resources-loader',
+                options: {
+                    resources: [
+                        `${__dirname}/sass/_resources.scss`
+                    ]
+                },
+            }
+        ]
       }
     ]
   },
   performance: { hints: false },
-  mode: process.env.NODE_ENV
+  mode: process.env.NODE_ENV,
+  plugins: [
+      new Dotenv()
+  ]
 };
